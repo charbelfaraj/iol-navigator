146402d931b4c9b9ae1f648021c287a182c53403
charbelfaraj <sharbelfaraj@live.com>
2026-03-19 15:22:42 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import IOLNavigator from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IOLNavigator />
+  </React.StrictMode>,
+)
